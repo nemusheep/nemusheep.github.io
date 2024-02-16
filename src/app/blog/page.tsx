d36a38457a8490e1ae9734292a5aee1bdd956681
nemusheep <miyomayo02@gmail.com>
2024-02-17 05:44:59 +0900

    next.jsなんもわからん

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,9 +5,9 @@ export default function BlogHome() {
   return (
     <main className={styles.main}>
       <p>blog pageだお</p>
-      <Link href='/0'>0</Link>
-      <Link href='/1'>1</Link>
-      <Link href='/2'>2</Link>
+      <Link href='/blog/0'>0</Link>
+      <Link href='/blog/1'>1</Link>
+      <Link href='/blog/2'>2</Link>
 
     </main>
   );
